f7ecb1141e5ff0f4d07a32fba73d398f5f76d0c6
Pierre Beitz <pibeitz@gmail.com>
2019-01-25 14:43:58 +0100

    Add a dummy constructor for tests

--- src/com/cloudbees/demo/Counter.groovy
+++ src/com/cloudbees/demo/Counter.groovy
@@ -1,9 +1,15 @@
 package com.cloudbees.demo
 
 class Counter {
+    def steps
     static count = 0
 
     static void increment() {
         count++;
+    }
+    
+    // dummy constructor for tests
+    Counter(def steps) {
+        this.steps = steps
     }
 }
