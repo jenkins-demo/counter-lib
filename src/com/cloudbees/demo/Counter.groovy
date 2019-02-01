5ebb3b6e9f7befcb9f7b8240f8ef8bc6f7ca0b38
Pierre Beitz <pibeitz@gmail.com>
2019-02-01 13:02:05 +0100

    tests

--- src/com/cloudbees/demo/Counter.groovy
+++ src/com/cloudbees/demo/Counter.groovy
@@ -1,15 +1,15 @@
 package com.cloudbees.demo
 
-class Counter {
+class Counter implements Serializable {
     def steps
     static count = 0
 
     static void increment() {
         count++;
     }
-    
+
     // dummy constructor for tests
-    Counter(def steps) {
-        this.steps = steps
+    Counter(Map m = [:]) {
+        this.steps = m.steps
     }
 }
